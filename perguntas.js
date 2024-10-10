ff145e768542ff5b66f3313d98cdb53d1a240f50
mariamartinez2007 <159436788+mariamartinez2007@users.noreply.github.com>
2024-10-10 11:13:12 -0300

    yu

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criarCartao(
-    'programaçao',
-    'o que e python?',
-    'o python e um Linguagem de programaçao'
+    'programação',
+    'o que é python?',
+    'o python é um Linguagem de programação'
 )
 
 criarCartao(
     'Geografia',
     'Qual a capital da frança?',
-    'A capital da frança e paris'
+    'A capital da frança é paris'
 )
 
 criarCartao(
-    'programaçao',
-    'o que e uma funçao?',
-    'uma funçao e um bloco de codigo que executa alguma tarefa'
+    'programação',
+    'o que e uma função?',
+    'uma função éyy um bloco de código que executa alguma tarefa'
 )
 
 criarCartao(
     'lingua inglesa',
-    'como se diz oi em Ingles?',
-    'oi em ingles e HI(RAI)'
+    'como se diz oi em Inglês?',
+    'oi em ingles é HI(RAI)'
 )
